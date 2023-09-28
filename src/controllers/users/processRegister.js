e20ab114c4a5738eb1fff36bd098a66bb6d98972
Eric <menaericdaniel@gmail.com>
2023-09-28 12:34:40 -0300

    feat : user register done!

--- src/controllers/users/processRegister.js
+++ src/controllers/users/processRegister.js
@@ -1,22 +1,32 @@
 const { validationResult } = require("express-validator");
-const { readJSON, writeJSON } = require("../../data");
-const User = require("../../data/User");
+const { hashSync } = require("bcryptjs");
+const db = require("../../database/models");
 
-module.exports = (req,res) => {
+module.exports = (req, res) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req);
-
-    if(errors.isEmpty()){
-        const users = readJSON('users.json');
-        const user = new User(req.body);
-    
-        users.push(user);
-        writeJSON(users,'users.json')
-    
-        return res.redirect('/')
-    }else {
-        return res.send(errors.mapped())
-    }
-    
- 
-}
+  if (errors.isEmpty()) {
+    const { name, surname, email, password } = req.body;
+    db.User.create({
+      name: name.trim(),
+      surname: surname.trim(),
+      email: email.trim(),
+      password: hashSync(password, 10),
+      roleId: 2,
+    })
+      .then((user) => {
+        console.log(user);
+        db.Address.create({
+          userId: user.id,
+        }).then(() => {
+          return res.redirect("/");
+        });
+      })
+      .catch((error) => console.log(error));
+  } else {
+    return res.render("register", {
+      old: req.body,
+      errors: errors.mapped(),
+    });
+  }
+};
