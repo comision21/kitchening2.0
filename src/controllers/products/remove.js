cffa6c779072c03120def7151195faa9e71a363e
Eric <menaericdaniel@gmail.com>
2023-10-27 12:33:12 -0300

    feat: add remove& fix:models

--- src/controllers/products/remove.js
+++ src/controllers/products/remove.js
@@ -1,13 +1,26 @@
+const { existsSync, unlinkSync } = require("fs");
 const db = require("../../database/models");
 
 module.exports = (req, res) => {
-  db.Product.destroy({
+  db.Image.findAll({
     where: {
-      id: req.params.id,
+      productId: req.params.id,
     },
   })
-    .then(() => {
-      return res.redirect("/admin");
+    .then((images) => {
+      images.forEach((image) => {
+        existsSync(`./public/img/products/${image.file}`) &&
+          unlinkSync(`./public/img/products/${image.file}`);
+      });
+
+      db.Product.destroy({
+        where: {
+          id: req.params.id,
+        },
+      }).then(() => {
+        return res.redirect("/admin");
+      });
     })
+
     .catch((error) => console.log(error));
 };
--- src/database/models/product.js
+++ src/database/models/product.js
@@ -41,7 +41,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Product',
-    paranoid: true,
+    //paranoid: true,
   });
   return Product;
 };
