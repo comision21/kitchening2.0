df73cf675464fecf490873e56a3985485f15a570
Eric <menaericdaniel@gmail.com>
2023-11-13 15:07:18 -0300

    feat: add product in cart advance

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,37 +1,79 @@
-const $ = id => document.getElementById(id)
+const $ = (id) => document.getElementById(id);
 
 const addItemToCart = async (quantity, product) => {
-    try {
-        const response = await fetch(`/api/cart/item?quantity=${quantity}&product=${product}`);
-        const result = await response.json();
-        console.log(result);
-        
-    } catch (error) {
-        console.error
+  try {
+    const response = await fetch(
+      `/api/cart/item?quantity=${quantity}&product=${product}`
+    );
+    const result = await response.json();
+    if (!result.ok) {
+      throw new Error(result.msg);
+    } else {
+      console.log(result);
     }
-}
+  } catch (error) {
+    Swal.fire({
+      title: "Upss",
+      text: "Hubo un error al acceder al carrito",
+      icon: "error",
+      html: `Debes <a class="link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover" href="/users/login">loguearte</a> para agregar datos al carrito`,
+    });
+  }
+};
 
+window.onload = function () {
+  $("btn-cart") &&
+    $("btn-cart").addEventListener("click", async function (e) {
+      try {
+        const response = await fetch("/api/cart");
+        const { ok, data } = await response.json();
 
-window.onload = function(){
+        if (ok) {
+          if (data.products.length) {
+            $("cart-body").innerHTML = `
+            <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">Imagen</th>
+                    <th scope="col">Product</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Cantidad</th>
+                    </tr>
+                </thead>
+                <tbody id="cart-table">
+                  
+                </tbody>
+                <caption>
+                    <div class="d-flex justify-content-end">
+                        <h5>Total: ${data.total}</h5> 
+                    </div>
+                 </caption>
+            </table>`;
 
-    $('btn-cart') && $('btn-cart').addEventListener('click', async function(e) {
-        try {
-
-            const response = await fetch('/api/cart');
-            const {ok,data} = await response.json();
-
-            if(ok){
-                if(data.products.length){
-                    $('cart-body').innerHTML = '<p>muestro los datos de los productos</p>'
-                }else {
-                    $('cart-body').innerHTML = '<div class="alert alert-warning" role="alert">No hay productos agregados al carrito</div>'
-                }
-            }
-            
-        } catch (error) {
-            console.error
+            data.products.forEach(({ image, title, price, quantity }) => {
+              $("cart-table").innerHTML += `
+            <tr>
+                <th scope="row"><img src="/img/products/${image}" alt="" width=100/></th>
+                <td>${title}</td>
+                <td>${price * quantity}</td>
+                <td>
+                    <div class="d-flex gap-2">
+                        <button class="btn btn-sm btn-danger"><i class="fa-solid fa-minus"></i></button>
+                        <input type="text" value=" ${quantity}" style="width:30px"/>
+                        <button class="btn btn-sm btn-success"><i class="fa-solid fa-plus"></i></button>
+                    </div>
+                </td>
+            </tr>
+            `;
+            });
+          } else {
+            $("cart-body").innerHTML =
+              '<div class="alert alert-warning" role="alert">No hay productos agregados al carrito</div>';
+          }
         }
-    })
-
-   
-}
+      } catch (error) {
+        console.error;
+        alert(error.message);
+      }
+    });
+};
--- src/controllers/APIs/cartApiController.js
+++ src/controllers/APIs/cartApiController.js
@@ -30,26 +30,39 @@ const addItemToCart = async (req, res) => {
       }
 
 
-    const { quantity, order, product } = req.query;
+    const { quantity, order, product : id } = req.query;
 
 
-    const { title, price, discount, images } = await db.Product.findByPk(product, {
+    const { title, price, discount, images } = await db.Product.findByPk(id, {
       include: ["images"],
     });
 
     console.log(images);
 
-
-    req.session.cart.products.push({
+    let newProduct = {
+      id,
       title,
       price,
       discount,
       image: images.find((image) => image.main).file,
-      quantity: quantity || 1,
-    });
+      quantity: +quantity || 1,
+    }
+
+    let resultSearch;
+
+    req.session.cart.products.map(product => {
+      if(product.id === id){
+        ++product.quantity
+        resultSearch = true
+      }
+    })
+
+    if(!resultSearch){
+      req.session.cart.products.push(newProduct);
+    }
 
     req.session.cart.total = req.session.cart.products
-      .map((product) => product.price)
+      .map((product) => product.price * product.quantity)
       .reduce((a, b) => a + b, 0);
 
     return res.status(200).json({
@@ -58,6 +71,7 @@ const addItemToCart = async (req, res) => {
     });
 
   } catch (error) {
+    console.log(error);
     return res.status(error.status || 500).json({
       ok: false,
       msg: error.message || "Upss, hubo un error :(",
